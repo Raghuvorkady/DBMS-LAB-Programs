6ca5206a829b49275b95da7a313ee1e00d2d2d12
Raghuvorkady <51401827+Raghuvorkady@users.noreply.github.com>
2020-12-17 11:19:33 +0530

    added query to drop tables in p5

---
+++ Oracle Express DB/program5/DROP.sql
@@ -0,0 +1,15 @@
+drop table works_on;
+
+drop table project;
+
+drop table dlocation;
+
+ALTER TABLE department drop column mgrssn;
+
+ALTER TABLE employee drop column dno;
+
+ALTER TABLE employee drop column superssn;
+
+drop table department;
+
+drop table employee;
